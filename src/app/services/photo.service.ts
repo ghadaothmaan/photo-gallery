927366a683b9dbc7edde149fc39d84ec63686a81
gee <ghadaothmaan@gmail.com>
2019-04-07 01:49:58 +0200

    fixed grid, stuck at photo preview

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
-  public photos: String[] = [
+  public photos: string[] = [
+    'assets/img/1.png',
+    'assets/img/2.png',
+    'assets/img/3.png',
+    'assets/img/1.png',
+    'assets/img/2.png',
+    'assets/img/3.png',
     'assets/img/1.png',
     'assets/img/2.png',
     'assets/img/3.png'
@@ -14,17 +21,24 @@ export class PhotoService {
 
   constructor(
     public toastController: ToastController,
-    public alertController: AlertController
+    public alertController: AlertController,
+    private photoViewer: PhotoViewer
   ) {}
 
+  // TODO: fix? sth to do with cordova and native ionic
+  // and guess what i cant get it work either
+  // but tbh i need to look more into it
+  // or maybe look into routing?
   openPreview() {
     console.log('clicked');
+    this.photoViewer.show(this.photos[0]);
   }
 
   // present toast with options
   async toastClicked() {
     const toast = await this.toastController.create({
       message:
+        // tslint:disable-next-line: quotemark
         "Boohoo~ Not done yet! Soon though! So here's a toast for now, cheers!",
       showCloseButton: true,
       position: 'bottom',
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,10 +9,13 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page {
   constructor(public photoService: PhotoService) {}
 
+  // TODO: implement native camera work
+  // and to do so cordova must work first and idk why it doesnt tbh!?
   takePicture() {
     this.photoService.toastClicked();
   }
 
+  // TODO: finish it ig
   previewPhoto() {
     this.photoService.openPreview();
   }
